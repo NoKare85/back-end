42623913f0252a8019ea4e42132ae3b585adb2a0
Niko Kareno <niko.kareno@tuni.fi>
2020-12-16 11:40:00 +0200

    Add express exercise to project

---
+++ express-test/Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@test.com',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Ben Doe',
+        email: 'ben@test.com',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Jim Doe',
+        email: 'jim@test.com',
+        status: 'inactive'
+    },
+]
+
+module.exports = members;
